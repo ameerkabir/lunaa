8331dcc0d17b85afb65bd9ad2b8bda0b1e16b21c
ameerkabir <ameerkabir2@gmail.com>
2021-05-27 22:47:35 +0100

    feat: add navigator component

---
+++ pages/navigator.jsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import Link from "next/link";
+import styled from "styled-components";
+import LoginForm from "./login";
+import SidBarBanner from "./SideBarBanner";
+
+const Container = styled.div`
+  display: flex;
+  /* justify-content: center;
+  justify-items: center;
+  align-items: center; */
+  height: 100vh;
+  padding: 16px;
+`;
+
+const LinkWrapper = styled(Link)`
+  margin: 8px;
+`;
+
+const MainHeader = styled.h1`
+  /* margin-bottom: 4px; */
+`;
+
+export default function Navigator({children}) {
+  return (
+    <>
+      <Container>
+        <SidBarBanner />
+        {children}
+      </Container>
+    </>
+  );
+}
